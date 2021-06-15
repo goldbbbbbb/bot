993eb2733fde20ce8ac0b4947bd51352f9205cf6
GoldBBBBBB <smilebestv3@gmail.com>
2021-06-15 22:10:33 +0800

    The lastest function 查刀 updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
 # IMPORT GSPREAD, ALLOWS ACCESS TO READ GOOGLE SHEETS.
 import discord
+from discord.enums import Status
 from discord.ext import commands
 import time
 import gspread
@@ -12,7 +13,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('pekodcbot.json',scope)
 client = gspread.authorize(creds)
-sheet = client.open('21/3月出刀紀錄').sheet1
+sheet = client.open('5月出刀紀錄').sheet1
 
 #prefix of the commands in server.
 bot = discord.ext.commands.Bot(command_prefix = "!")
@@ -23,6 +24,9 @@ messages = {}
 def is_in_channel(ctx, channel_name):
   return ctx.channel.name == channels[channel_name].name
 
+#Set variable of round
+round = 1
+
 #Confirm message when bot succesfully run.
 @bot.event
 async def on_ready():
@@ -32,6 +36,7 @@ async def on_ready():
   channels_test["table"] = bot.get_channel(817829173069348884)
   channels_test["admin"] = bot.get_channel(818157150232248341)
   messages_test["table"] = await channels_test["table"].fetch_message(817834334365941761)
+  
 
   #817674249699459072 掛樹頻->報刀頻 771259390241669132
   #817829173069348884 刀表->報刀表 818898908407267338
@@ -40,6 +45,8 @@ async def on_ready():
   channels["command"] = bot.get_channel(771259390241669132)
   channels["table"] = bot.get_channel(818898908407267338)
   channels["admin"] = bot.get_channel(817851835955281930)
+  channels["discuss"] = bot.get_channel(815937067342888960)
+  channels["group"] = bot.get_channel(815937067342888960)
 
   messages["table"] = await channels["table"].fetch_message(818904471606132767)
 
@@ -72,15 +79,11 @@ async def showchannels(ctx):
 #Check rest of the num of knife !num
 @bot.command()
 async def num(ctx):
-<<<<<<< HEAD
-  #let user can use !num in any channel
-=======
   #Let user can use !num in any channel
->>>>>>> development
   #if not is_in_channel(ctx, "command"):
     #return
-  #get the data in (38,17) of first sheet of the google sheets we assigned.
-  result = sheet.cell(38,17).value
+  #get the data in (94,2) of first sheet of the google sheets we assigned.
+  result = sheet.cell(94,2).value
   await ctx.send(f'目前仍有 {result} 刀未出。')
 
 #BookBoss !book
@@ -113,7 +116,7 @@ class KnifeRequests:
     self.normals = []
 
   def __repr__(self):
-    return f'\n長補: {self.extras} \n正刀: {self.normals}'
+    return f'長補: {self.extras}\n\n---------------\n\n正刀: {self.normals}'
 
   def __getstate__(self):
     return self.__dict__
@@ -121,9 +124,15 @@ class KnifeRequests:
   def __setstate__(self, d):
     self.__dict__ = d
 
-  def apply_filter(self, filter_func):
-    self.extras = list(filter(filter_func, self.extras))
-    self.normals = list(filter(filter_func, self.normals))
+  def remove_user_first_occ(self, target_username):
+    for request in self.extras:
+      if request.user.display_name == target_username:
+        self.extras.remove(request)
+        break
+    for request in self.normals:
+      if request.user.display_name == target_username:
+        self.normals.remove(request)
+        break
 
   def add(self, request, is_extra):
     if is_extra:
@@ -142,24 +151,35 @@ class KnifeRequests:
 boss_numbers = ['短補', '一王', '二王', '三王', '四王', '五王']
 knife_requests = [[]] + [KnifeRequests() for _ in range(len(boss_numbers)-1)]
 
+# HP for each boss
+stage12 = [0,6000000,8000000,10000000,12000000,15000000]
+stage3 = [0,7000000,9000000,13000000,15000000,20000000]
+stage4 = [0,17000000,18000000,20000000,21000000,23000000]
+stage5 = [0,85000000,90000000,95000000,100000000,110000000]
+bosshp = 0
+remainhp = 0
+
 @bot.command()
-<<<<<<< HEAD
-async def b(ctx, boss, numknife, damage, notice="無", extra=False):
-=======
 async def b(ctx, boss, damage, notice="無", extra=False):
->>>>>>> development
   if not is_in_channel(ctx, "command"):
     return
 
-  msg = ""
+  await b_content(ctx, boss, damage, notice="無", extra=False)
+
+async def b_content(ctx, boss, damage, notice="無", extra=False):
+
+  msg = ''
 
   try:
     boss = int(boss)
+    if (bool(extra)):
+      msg = f'{ctx.author.mention} ' + ('補償刀' if bool(extra) else '') + f'預約 {boss_numbers[boss]} , 秒數: {damage}秒, 備注: {notice}'
+    else:
+      msg = f'{ctx.author.mention} ' + f'預約 {boss_numbers[boss]} , 傷害: {damage}, 備注: {notice}'
     if boss > 0:
       knife_requests[boss].add(KnifeRequest(ctx.author, boss, damage, notice), bool(extra))
     else:
       knife_requests[boss].append(KnifeRequest(ctx.author, boss, damage, notice))
-    msg = f'{ctx.author.mention} ' + ('補償刀' if bool(extra) else '') + f'預約 {boss_numbers[boss]} , 傷害: {damage}, 備注: {notice}'
   except:
     #raise ValueError
     msg = '請按照 !book [幾王] [傷害] [備注] 的方式報刀'
@@ -170,46 +190,40 @@ async def b(ctx, boss, damage, notice="無", extra=False):
     #from traceback import print_stack
     #print_stack()
 
+
   await ctx.send(msg)
   await update_table()
 
 # Requests for each boss (long make up time) !extra
 @bot.command()
-<<<<<<< HEAD
-async def eb(ctx, boss, numknife, damage, notice="無"):
-  await b(ctx, boss, numknife, damage, notice, True)
-
-#CancelBooking !unbook
-@bot.command()
-async def ub(ctx, numknife):
-=======
 async def eb(ctx, boss, damage, notice="無"):
-  await b(ctx, boss, damage, notice, True)
+  await b_content(ctx, boss, damage, notice, extra=True)
 
 #CancelBooking !unbook
 @bot.command()
 async def ub(ctx, bossnum):
->>>>>>> development
   if not is_in_channel(ctx, "command"):
     return
 
-  def filter_function(knife_request):
-    if ctx.author.display_name != knife_request.user.display_name:
-      return True # All non-author entries must stay
-    return knife_request.boss != int(bossnum) # requests with different bossnum stay
+  await ub_content(ctx, int(bossnum))
 
-  # Loop through all bosses
-  for boss in range(len(knife_requests)):
-    if boss == 0:
-      knife_requests[boss] = list(filter(filter_function, knife_requests[boss]))
-    else:
-      knife_requests[boss].apply_filter(filter_function)
+async def ub_content(ctx, bossnum):
+
+  target_username = ctx.author.display_name
+  bossnum = int(bossnum)
+  if bossnum == 0: # knife_requests[0] is a list
+    for request in knife_requests[bossnum]:
+      if request.user.display_name == target_username:
+        knife_requests[bossnum].remove(request)
+        break
+  else: # knife_requests[boss] is a KnifeRequests object
+    knife_requests[bossnum].remove_user_first_occ(target_username)
   await ctx.send(f'{ctx.author.mention} 已取消所有 {bossnum}王 的報刀。')
   await update_table()
 
 #CancelBookingforguildmember !unbookfor
 @bot.command()
-async def ubf(ctx, boss, mention):
+async def ubf(ctx, bossnum, mention):
   if not is_in_channel(ctx, "admin"):
     return
 
@@ -218,14 +232,25 @@ async def ubf(ctx, boss, mention):
   def filter_for_non_author(knife_request):
     return target_user.display_name != knife_request.user.display_name
   
-  boss = int(boss)
+  # boss = int(boss)
 
-  if boss == 0:
-    knife_requests[boss] = list(filter(filter_for_non_author, knife_requests[boss]))
-  else:
-    knife_requests[boss].apply_filter(filter_for_non_author)
-  await ctx.send(f'{ctx.author.mention} 已取消 {boss} 王中 {target_user.mention} 的報刀。')
-  await channels['command'].send(f'幹部 {ctx.author.mention} 已取消 {boss} 王中 {target_user.mention} 的報刀。')
+  # Loop through all bosses
+  target_username = ctx.author.display_name
+  bossnum = int(bossnum)
+  if bossnum == 0: # knife_requests[0] is a list
+    for request in knife_requests[bossnum]:
+      if request.user == target_username:
+        knife_requests[bossnum].remove(request)
+        break
+  else: # knife_requests[boss] is a KnifeRequests object
+    knife_requests[bossnum].remove_user_first_occ(target_username)
+
+  #if boss == 0:
+    #knife_requests[boss] = list(filter(filter_for_non_author, knife_requests[boss]))
+  #else:
+    #knife_requests[boss].apply_filter(filter_for_non_author)
+  await ctx.send(f'{ctx.author.mention} 已取消 {bossnum} 王中 {target_user.mention} 的報刀。')
+  await channels['command'].send(f'幹部 {ctx.author.mention} 已取消 {bossnum} 王中 {target_user.mention} 的報刀。')
   await update_table()
 
 #SeekHelp !sos
@@ -250,20 +275,24 @@ async def sos(ctx):
   #if not is_in_channel(ctx, "command"):
     #return
   sos_users.add(SosPlayer(ctx.author.mention, ctx.author.display_name))
-  await ctx.send(f'{ctx.author.mention} 掛樹了，有人能幫幫他嗎？現時樹上人數: {len(sos_users)}')
+  await channels["discuss"].send(f'{ctx.author.mention} 掛樹了，有人能幫幫他嗎？現時樹上人數: {len(sos_users)}')
   await update_table()
 
 #Nextboss !next
 current_boss = 1
 last_run_next = time.time()
 NEXT_COOLDOWN = 1 # 60s
+
 @bot.command()
 async def next(ctx):
   if not is_in_channel(ctx, "command"):
     return
 
+  await next_content(ctx)
+
+async def next_content(ctx):
   # Show current/next boss info
-  global current_boss, last_run_next
+  global current_boss, last_run_next, round
   current_time = time.time()
   if current_time <= last_run_next + NEXT_COOLDOWN:
     await ctx.send(f'此指令還有 {int(NEXT_COOLDOWN - (current_time-last_run_next))} 秒冷卻。')
@@ -274,31 +303,174 @@ async def next(ctx):
   # print(type(knife_requests))
   for request in knife_requests[current_boss].normals + knife_requests[current_boss].extras:
     next_users += f'{request.user.mention}({request.damage}, {request.notice}) '
-  await ctx.send(f'現在到 {boss_numbers[current_boss]}。 {next_users}')
+  await channels["discuss"].send(f'現在到 {boss_numbers[current_boss]}。 {next_users}')
+  # increase the round when current_boss back to 1
+  if current_boss == 1:
+    round = round + 1
+  await update_table()
 
-  # Tag all sos users
-  user_names = ""
-  for user in sos_users:
-    user_names += user.mention + ' '
-  if user_names != "":
-    await ctx.send(f'{user_names} 已經到下一隻王了，可以下樹囉。')
-    sos_users.clear()
-    print(sos_users)
+reports = {}
+remainknife = {}
+namelist = {}
+resetknife = {}
+
+#Register as guild member
+@bot.command()
+async def reg(ctx, gameid):
+  if remainknife.__contains__(ctx.author.id):
+    await ctx.send(f'{ctx.author.mention} 你已經登錄過了，不需要再登錄一次。')
+  else:
+    remainknife[ctx.author.id] = 3
+    resetknife[ctx.author.id] = 3
+    namelist[ctx.author.id] = gameid
+    await ctx.send(f'{ctx.author.mention} 已登錄完成，登錄ID為：{gameid}。')
+
+#Unregister for change the id
+@bot.command()
+async def unreg(ctx):
+  if remainknife.__contains__(ctx.author.id):
+    del remainknife[ctx.author.id]
+    await ctx.send(f'{ctx.author.mention} 已取消登錄。')
+  else:
+    await ctx.send(f'{ctx.author.mention} 你並未有登錄紀錄。')
+
+#reset the num of knife(when 5:00)
+@bot.command()
+async def reset(ctx):
+  remainknife.update(resetknife)
+
+#Check the remain knife real time
+@bot.command()
+async def 查刀(ctx):
+  remain = f'```目前剩餘刀數：\n\n'
+  for dcid, num in remainknife.items():
+    dcname = namelist[dcid]
+    remain += f'{dcname}    剩餘{num}刀\n'
+  remain += '```'
+  await ctx.send(remain)
+
+#Tell the control field that you are going into the fight
+@bot.command()
+async def 進(ctx):
+  if not is_in_channel(ctx, "group"):
+    return
+
+  #check if the user used this function in same boss or not
+  if (ctx.author.display_name in reports):
+    await ctx.send(f'{ctx.author.mention} 你已經報過進刀了，不用再報一次。')
+
+  #import user display name into the list
+  else:
+    reports[ctx.author.display_name] = ''
+    await ctx.send(f'{ctx.author.mention} 已進場打 {boss_numbers[current_boss]}。')
+
+@bot.command()
+async def 報(ctx, msg):
+  if not is_in_channel(ctx, "group"):
+    return
+
+  #check if the user used 進 function or not
+  if (ctx.author.display_name not in reports):
+    await ctx.send(f'{ctx.author.mention} 你還沒報進刀，是要怎麼報正式戰狀況？')
+
+  else:
+    reports[ctx.author.display_name] = msg
+    print(reports)
+    await ctx.send(f'{ctx.author.mention} 已回報：{msg}')
+
+#check the status of guild member
+@bot.command()
+async def stat(ctx):
+  stat = f'```目前進刀狀況：\n\n'
+  for user, msg in reports.items():
+    stat += f'{user} 已進場，報：{msg}\n'
+  stat += '```'
+  await ctx.send(stat)
+
+#for guild member report damage
+@bot.command()
+async def 出(ctx, damage):
+  if not is_in_channel(ctx, "group"):
+    return
+
+  global current_boss, last_run_next, round, remainhp, bosshp, reports
+  if round < 11:
+    bosshp = int(stage12[current_boss])
+  elif round < 35:
+    bosshp = int(stage3[current_boss]) 
+  elif round < 45:
+    bosshp = int(stage4[current_boss]) 
+  else:
+    bosshp = int(stage5[current_boss])
+
+  if remainhp == 0:
+    remainhp = bosshp - int(damage)
+  else:
+    remainhp = remainhp - int(damage)
+
+  if remainhp > 0:
+    await ctx.send(f'{ctx.author.mention} 已出場，對 {boss_numbers[current_boss]} 造成了{damage}點傷害，目前王還剩 {remainhp} 點血。')
+    remainknife[ctx.author.id] = remainknife[ctx.author.id] - 1
+    
+    #run the ub function
+    await ub_content(ctx, current_boss)
+
+    del reports[ctx.author.display_name]
+
+  else:
+    # show the bossisdead msg
+    await ctx.send(f'{ctx.author.mention} 已出場，對 {boss_numbers[current_boss]} 造成了{damage}點傷害並收王。')
+    await ctx.send(f'{ctx.author.mention} 請使用指令 !尾 <秒數> <幾王>，若使用補償刀收王請輸入 !尾 0 0')
   
   await update_table()
 
+#for guild member report the extra second
+@bot.command()
+async def 尾(ctx, time, bossnum=0):
+  if not is_in_channel(ctx, "group"):
+    return
+
+  global reports
+  
+  #run the ub function
+  await ub_content(ctx, current_boss)
+
+  #run the eb function if the collecter is 正刀
+  time = int(time)
+  bossnum = int(bossnum)
+  if (time != 0):
+    remainknife[ctx.author.id] = remainknife[ctx.author.id] - 1
+    if (bossnum != 0):
+      await b_content(ctx, bossnum, time, extra=True)
+    else:
+      await b_content(ctx, 0, time, extra=True)
+  else:
+    if (bossnum < 0 or bossnum > 6):
+      await ctx.send(f'{ctx.author.mention} 請按照 !尾 <秒數> <幾王> 的格式使用指令')
+
+  # run the next function
+  # Show current/next boss info
+  await next_content(ctx)
+
+  stat = f'```目前進刀狀況：\n\n'
+  stat += '```'
+  reports = {}
+
 #Setbossnum !set
 @bot.command()
-async def set(ctx, bossnum):
+async def set(ctx, roundnum, bossnum):
   if not is_in_channel(ctx, "admin"):
     return
+
+  global round, remainhp
 
   try:
       if int(bossnum) > 0 and int(bossnum) < 6:
         global current_boss
         current_boss = int(bossnum)
-        await channels["command"].send(f'經幹部 {ctx.author.mention} 調整，現在到 {boss_numbers[current_boss]}。')
-        await ctx.send(f'{ctx.author.mention} 已強行調整至 {boss_numbers[current_boss]}')
+        round = int(roundnum)
+        await channels["command"].send(f'經幹部 {ctx.author.mention} 調整，現在到 {round}周{boss_numbers[current_boss]}。')
+        await ctx.send(f'{ctx.author.mention} 已強行調整至 {round}周{boss_numbers[current_boss]}')
         await update_table()
       else:
         await ctx.send(f'請按照 !set [幾王] 的方式調整目前幾王。')
@@ -314,17 +486,61 @@ async def update_table():
       formatted += f'{info}, '
     return formatted
 
-  table = ''
+  table = f'**目前到 {round}周{boss_numbers[current_boss]}**\n\n==============================\n'
   for i in range(1, len(boss_numbers)):
     if i == current_boss:
-      table += f'**{boss_numbers[i]}:\n {knife_requests[i]}**\n==============================\n'
+      table += f'\n> **>>>>>  {boss_numbers[i]}，剩餘血量 {remainhp}  <<<<<**\n```fix\n{knife_requests[i]}\n```\n'
     else:
-      table += f'{boss_numbers[i]}:\n {knife_requests[i]}\n==============================\n' # 一至五王
+      table += f'\n{boss_numbers[i]}\n```{knife_requests[i]}\n```\n' # 一至五王
   table += ''
-  table += f'{boss_numbers[0]}: {format_list_infos(knife_requests[0])}\n==============================\n' # 補償
-  table += f'樹上: {format_list_infos(sos_users) if len(sos_users)>0 else ""}\n'
+  table += f'\n{boss_numbers[0]}: {format_list_infos(knife_requests[0])}\n' # 補償
+  table += f'\n樹上: {format_list_infos(sos_users) if len(sos_users)>0 else ""}\n'
   table += ''
   await messages["table"].edit(content=table)
+
+# meme set of the guild
+
+# @bot.event
+# async def on_message(message):
+#     if 'emperorthinking' in message.content:
+#       await message.channel.send(f"{message.author.mention} 你是不是找我麻煩？")
+#     else:
+#       await bot.process_commands(message)
+
+#     if 'gay' in message.content:
+#       await message.channel.send(file=discord.File('C:/Users/GB/Desktop/meme/gay2.png'))
+#     else:
+#       await bot.process_commands(message)
+
+#     if '成功人士' in message.content:
+#       await message.channel.send(file=discord.File('C:/Users/GB/Desktop/meme/success.jpg'))
+#     else:
+#       await bot.process_commands(message)
+
+#     if '失敗人士' in message.content:
+#       await message.channel.send(file=discord.File('C:/Users/GB/Desktop/meme/loser.png'))
+#     else:
+#       await bot.process_commands(message)
+
+#     if 'her' in message.content:
+#       await message.channel.send(file=discord.File('C:/Users/GB/Desktop/meme/her.png'))
+#     else:
+#       await bot.process_commands(message)
+
+#     if '保150' in message.content:
+#       await message.channel.send(file=discord.File('C:/Users/GB/Desktop/meme/150.png'))
+#     else:
+#       await bot.process_commands(message)
+
+#     if '盡力' in message.content:
+#       await message.channel.send(file=discord.File('C:/Users/GB/Desktop/meme/盡力.png'))
+#     else:
+#       await bot.process_commands(message)
+
+#     if '背刺' in message.content:
+#       await message.channel.send(file=discord.File('C:/Users/GB/Desktop/meme/背刺.png'))
+#     else:
+#       await bot.process_commands(message)
 
 # Save and Load everything in the table (knife_requests and sos_users)
 var_to_save = ['knife_requests', 'sos_users']
